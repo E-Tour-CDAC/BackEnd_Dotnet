ae28ec009f66b210dd4c20d50f181f9d0dc5f8f4
mohitjp-dev <patilmj.123@gmail.com>
2026-02-02 23:43:50 +0530

    dependencies added

--- Backend_dotnet/Data/AppDbContext.cs
+++ Backend_dotnet/Data/AppDbContext.cs
@@ -40,7 +40,6 @@ public partial class AppDbContext : DbContext
     public virtual DbSet<tour_master> tour_master { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseMySql("server=localhost;database=${DB_URL};user=${DB_USER};password=${DB_PASS}", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.43-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
